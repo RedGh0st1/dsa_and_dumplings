c243bddbe40ba71cd087dcf7a545cbfb8b9baa78
Lennie Nurse <lnursej@gmail.com>
2024-10-09 18:54:30 -0400

    update needs to be finish

--- stacks/min_stack.js
+++ stacks/min_stack.js
@@ -1,5 +1,21 @@
 class MinStack {
-  constructor() {}
+  constructor() {
+    this.stack = [];
+    this.min = null;
+  }
+  push(value) {
+    if (this.min === null || value < this.min) {
+      this.min = value;
+    }
+    this.stack.push(value);
+  }
+
+  top(value) {
+    return this.stack(this.stack.length - 1);
+  }
+  getMin() {
+    return this.min;
+  }
 }
 
 module.exports = MinStack;
